79234c2be9a149b534855bf2c441731b6cb4b592
Ilyas Sonmez <lunap.ilyas@gmail.com>
2026-01-28 16:48:46 +0300

    MongoDb Bağlantısı Yapıldı

---
+++ AkademiQMongoDb/AkademiQMongoDb/Settings/IDatabaseSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace AkademiQMongoDb.Settings
+{
+    public class IDatabaseSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        public string CategoryCollectionName { get; set; }
+        public string ProductCollectionName { get; set; }
+    }
+}
