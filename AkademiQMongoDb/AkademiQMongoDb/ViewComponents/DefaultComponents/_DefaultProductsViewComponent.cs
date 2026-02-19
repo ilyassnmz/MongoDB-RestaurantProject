a558602ae66b4ca036ddcec0c8ad2e9cd55473cf
Ilyas Sonmez <lunap.ilyas@gmail.com>
2026-02-19 22:18:33 +0300

    UI Tarafında güncellemeler

---
+++ AkademiQMongoDb/AkademiQMongoDb/ViewComponents/DefaultComponents/_DefaultProductsViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using AkademiQMongoDb.Services.ProductServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AkademiQMongoDb.ViewComponents.DefaultComponents
+{
+    public class _DefaultProductsViewComponent : ViewComponent
+    {
+        private readonly IProductService _productService;
+
+        public _DefaultProductsViewComponent(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+
+
+        public async Task<IViewComponentResult> Invoke()
+        {
+            var products = await _productService.GetAllAsync();
+            return View(products);
+        }
+    }
+}
