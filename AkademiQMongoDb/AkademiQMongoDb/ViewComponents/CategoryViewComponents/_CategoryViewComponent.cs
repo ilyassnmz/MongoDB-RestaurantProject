5587e3b3d3544e10045c14a020e6371879e94c25
Ilyas Sonmez <lunap.ilyas@gmail.com>
2026-02-20 20:23:35 +0300

    UI Güncellendi

---
+++ AkademiQMongoDb/AkademiQMongoDb/ViewComponents/CategoryViewComponents/_CategoryViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AkademiQMongoDb.ViewComponents
+{
+    public class _CategoryViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
---
+++ AkademiQMongoDb/AkademiQMongoDb/ViewComponents/TestimonialViewComponent/_TestimonialViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AkademiQMongoDb.ViewComponents
+{
+    public class _TestimonialViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
