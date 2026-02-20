9ca7a37db9033817bf8a0fc66b232d62856abafc
Ilyas Sonmez <lunap.ilyas@gmail.com>
2026-02-20 12:18:05 +0300

    LoginPage Eklendi

---
+++ AkademiQMongoDb/AkademiQMongoDb/Controllers/RegisterController.cs
@@ -0,0 +1,23 @@
+﻿using AkademiQMongoDb.DTOs.AdminDtos;
+using AkademiQMongoDb.Services.AdminServices;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AkademiQMongoDb.Controllers
+{
+    [AllowAnonymous]
+    public class RegisterController(IAdminService _adminService) : Controller
+    {
+        public IActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Signup(RegisterAdminDto registerAdminDto)
+        {
+            await _adminService.CreateAdminAsync(registerAdminDto);
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
